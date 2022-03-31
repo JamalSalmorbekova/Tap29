f44f03d89bb627436ef6f5d2b867d2546d02ef56
Jamal <jamalpeaksoft@gmail.com>
2022-03-31 12:50:39 +0600

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,10 +9,14 @@ import java.util.stream.Collectors;
     public class Main {
 
     public static void main(String[] args) {
-	List<Integer>integers = Arrays.asList(4,24,13,12,36,19,25,31,28,10,22,39,42,16);
-    integers.stream().filter(a -> a%2==0).map(a-> a*a).
-            max(Integer::compare).ifPresent(System.out::println);
+        int[] array1 = new int[]{4, 24, 13, 12, 36, 19, 25, 31, 28, 10, 22, 39, 42, 16};
+        List<Integer> integers = Arrays.asList(4, 24, 13, 12, 36, 19, 25, 31, 28, 10, 22, 39, 42, 16);
+        integers.stream().filter(a -> a % 2 == 0).map(a -> a * a).
+                max(Integer::compare).ifPresent(System.out::println);
+        System.out.println(array1.length);
+        System.out.println("Элементы  массива :");
+        for (int i = 0; i < 14; i++) {
 
-    }
+            System.out.println(array1[i]);  }
 
-}
+    }}
